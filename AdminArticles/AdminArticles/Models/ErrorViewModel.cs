1613e0f938a77cb585b27190c9f174b7c0c7cd8f
Dany <danysaintlouis44@yahoo.fr>
2024-05-30 14:19:52 +0200

    modification projet

---
+++ AdminArticles/AdminArticles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminArticles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
